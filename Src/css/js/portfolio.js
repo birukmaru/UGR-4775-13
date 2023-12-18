fd17e5e33e907255892ab8882da4ca10b9f566a7
birukmaru <147934036+birukmaru@users.noreply.github.com>
2023-12-18 19:50:06 +0300

    final edit portfolio.js

--- Src/css/js/portfolio.js
+++ Src/css/js/portfolio.js
@@ -20,25 +20,6 @@ function sendMail() {
   }
 
 
-
-  document.addEventListener("DOMContentLoaded", function () {
-    var $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll(".navbar-burger"),
-      0
-    );
-    if ($navbarBurgers.length > 0) {
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener("click", function () {
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-          $el.classList.toggle("is-active");
-          $target.classList.toggle("is-active");
-        });
-      });
-    }
-  });
-
-
   function downloadResume() {
     var resumeUrl = "images/Resume.pdf";
     var link = document.createElement("a");
